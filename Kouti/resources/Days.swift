564537772b2e188cdd948baa57442f5f936d0c27
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-06-18 16:40:19 -0300

    .gitignore adicionado

---
+++ Kouti/resources/Days.swift
@@ -0,0 +1,18 @@
+//
+//  Days.swift
+//  Kouti
+//
+//  Created by Thiago Medeiros on 18/06/21.
+//
+
+import Foundation
+
+enum Days: String {
+    case sunday = "d",
+         monday = "s",
+         tuesday = "t",
+         wednesday = "q",
+         thursday = "q",
+         friday = "s",
+         saturday = "s"
+}
