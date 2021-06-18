384e438c4ddc1fddcfd4b00f8904c0c922a46be3
Marco Zulian <marco.ab.zulian@gmail.com>
2021-06-18 16:49:27 -0300

    Correção do nome da classe MonsterModel (estava MosterModel) e criação de BestiaryModel

---
+++ Kouti/models/BestiaryModel.swift
@@ -0,0 +1,12 @@
+//
+//  BestiaryModel.swift
+//  Kouti
+//
+//  Created by Marco Zulian on 18/06/21.
+//
+
+import Foundation
+
+struct Bestiary {
+    var monsterColletion: [MonsterModel: Int]
+}
--- Kouti/models/MonsterModel.swift
+++ Kouti/models/MonsterModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MosterModel {
+struct MonsterModel {
     let name: String
     let description: String = ""
     let category: Category
