3d9db85914df7be39551e9f16ecd9933da364faa
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-06-18 15:19:04 -0300

    Initial Commit

---
+++ Kouti/KoutiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoutiApp.swift
+//  Kouti
+//
+//  Created by Thiago Medeiros on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct KoutiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
