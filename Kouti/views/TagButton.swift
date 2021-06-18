9ed4aeeb3560a901d329eb57659c1d360a9e0e44
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-06-18 20:56:46 -0300

    TagButton como um botão

--- Kouti/views/TagButton.swift
+++ Kouti/views/TagButton.swift
@@ -12,33 +12,37 @@ struct TagButton: View {
 
     let category: Category
     let hasBorder: Bool
+    let action: () -> Void
     
-    init(_ category: Category, hasBorder: Bool = true) {
+    init(_ category: Category, hasBorder: Bool = true, action: @escaping () -> Void) {
         self.category = category
         self.hasBorder = hasBorder
+        self.action = action
     }
     
     var body: some View {
-        Text(category.rawValue)
-            .font(.body)
-            .fontWeight(.semibold)
-            .foregroundColor(category.getColor())
-            .padding(.horizontal, 16)
-            .padding(.vertical, 6)
-            .background(
-                RoundedRectangle(cornerRadius: 50)
-                    .fill(colorScheme == .dark ? Color("light1"): Color("light4"))
-                    .shadow(color: Color("dark4"), radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1)
-            )
-            .overlay(
-                RoundedRectangle(cornerRadius: 40)
-                    .stroke(category.getColor(), lineWidth: self.hasBorder ? 2 : 0)
-            )
+        Button(action: action, label: {
+            Text(category.rawValue)
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(category.getColor())
+                .padding(.horizontal, 16)
+                .padding(.vertical, 6)
+                .background(
+                    RoundedRectangle(cornerRadius: 50)
+                        .fill(colorScheme == .dark ? Color("light1"): Color("light4"))
+                        .shadow(color: Color("dark4"), radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(category.getColor(), lineWidth: self.hasBorder ? 2 : 0)
+                )
+        })
     }
 }
 
 struct TagButton_Previews: PreviewProvider {
     static var previews: some View {
-        TagButton(.health, hasBorder: true)
+        TagButton(.health, hasBorder: true, action: {})
     }
 }
