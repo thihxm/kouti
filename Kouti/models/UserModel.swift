2c838c4fb69827693d6b02088e5d0fb45b660911
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-06-18 16:58:16 -0300

    Adicionando o modelo do usuário

---
+++ Kouti/models/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModels.swift
+//  Kouti
+//
+//  Created by Thiago Medeiros on 18/06/21.
+//
+
+import Foundation
+
+struct UserModels {
+    let character: CharacterModel
+    let tasks: [TaskModel]
+    let streak: Int    
+}
--- Kouti/models/UserModels.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserModels.swift
-//  Kouti
-//
-//  Created by Thiago Medeiros on 18/06/21.
-//
-
-import Foundation
