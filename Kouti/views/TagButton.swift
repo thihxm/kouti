794fa42b02797323ae755c7a492031b7767638b5
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-06-21 12:33:26 -0300

    Customização do TagButton sem bordas

--- Kouti/views/TagButton.swift
+++ Kouti/views/TagButton.swift
@@ -9,27 +9,52 @@ import SwiftUI
 
 struct TagButton: View {
     @Environment(\.colorScheme) var colorScheme
+    @State var isActive: Bool = false
 
     let category: Category
     let hasBorder: Bool
     let action: () -> Void
     
-    init(_ category: Category, hasBorder: Bool = true, action: @escaping () -> Void) {
+    init(_ category: Category, hasBorder: Bool = false, action: @escaping () -> Void) {
         self.category = category
         self.hasBorder = hasBorder
         self.action = action
     }
     
+    func getButtonBackgroundColor() -> Color {
+        if hasBorder {
+            if isActive {
+                return category.getColor()
+            }
+            return colorScheme == .dark ? Color("light1") : Color("light4")
+        }
+        return isActive ? Color("bg1") : Color("grayBgButtons")
+    }
+    
+    func getTextColor() -> Color {
+        if hasBorder {
+            if isActive {
+                return colorScheme == .dark ? Color("light1") : Color("light4")
+            }
+            return category.getColor()
+        }
+        return Color("dark2")
+    }
+    
+    func onClick() {
+        isActive.toggle()
+        action()
+    }
+    
     var body: some View {
-        Button(action: action,label: {
+        Button(action: onClick,label: {
             Text(category.rawValue)
                 .font(.body)
-                .foregroundColor(category.getColor())
+                .foregroundColor(getTextColor())
                 .fontWeight(.semibold)
                 .padding(.horizontal, 16)
                 .padding(.vertical, 6)
-                .background(colorScheme == .dark ? Color("light1"): Color("light4")
-                )
+                .background(getButtonBackgroundColor())
                 .clipShape(RoundedRectangle(cornerRadius: 50))
                 .shadow(color: Color("dark4"), radius: 4, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1)
                 .overlay(
@@ -42,7 +67,7 @@ struct TagButton: View {
 
 struct TagButton_Previews: PreviewProvider {
     static var previews: some View {
-        TagButton(.health, hasBorder: true, action: {})
+        TagButton(.health, hasBorder: false, action: {})
             
     }
 }
